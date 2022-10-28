adbd92dcbec7cbdce63c999c727dc408a350f9a1
ujjwal <ujjwal.kumar579@gmail.com>
2022-10-29 00:41:49 +0530

    today work done

--- src/components/Astro_detail/Astro_detail.jsx
+++ src/components/Astro_detail/Astro_detail.jsx
@@ -1,15 +1,43 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import "./Astro_detail.css";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 const Astro_detail = () => {
-
   const myData = useSelector((state) => state.data);
-  console.log(myData.getApiData);
+  // console.log(myData.getApiDataBirth);
+  // console.log(myData.getApiDataAstro);
+  // console.log(myData.getApiDataPanchang);
 
   return (
     <div>
-      <h1>Astro details</h1>
+      <Container fluid>
+        <Row>
+          <Col>
+            <div className="tabs">
+              <Tabs
+                defaultActiveKey="profile"
+                id="fill-tab-example"
+                className="mb-3"
+                fill
+                variant="pills"
+              >
+                <Tab eventKey="home" title="Home">
+                  <h1>{myData.getApiDataBirth.sunrise}</h1>
+                </Tab>
+                <Tab eventKey="profile" title="Profile">
+                  <h1>{myData.getApiDataAstro.ascendant_lord}</h1>
+                </Tab>
+                <Tab eventKey="longer-tab" title="Loooonger Tab">
+                  <h1>{myData.getApiDataPanchang.tithi}</h1>
+                </Tab>
+              </Tabs>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
